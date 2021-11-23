33dc202860bacb08366ae100439602ff9010e20d
kjetilh33 <kjetil.halvorsen@cognite.com>
2021-11-23 22:17:31 +0100

    Add source interface.

--- src/main/java/com/cognite/client/Events.java
+++ src/main/java/com/cognite/client/Events.java
@@ -23,6 +23,7 @@ import com.cognite.client.config.ResourceType;
 import com.cognite.client.servicesV1.ConnectorServiceV1;
 import com.cognite.client.servicesV1.parser.EventParser;
 import com.cognite.client.config.UpsertMode;
+import com.cognite.client.stream.Source;
 import com.google.auto.value.AutoValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,7 +37,7 @@ import java.util.stream.Collectors;
  * It provides methods for reading and writing {@link Event}.
  */
 @AutoValue
-public abstract class Events extends ApiBase {
+public abstract class Events extends ApiBase implements Source<Event> {
 
     private static Builder builder() {
         return new AutoValue_Events.Builder();
