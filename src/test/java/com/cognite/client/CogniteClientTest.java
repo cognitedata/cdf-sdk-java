54a676f05f01cc3acffe68cbbd3d99f88d0d4361
Rodrigo Ornellas <rodrigo.ornellas@cognite.com>
2022-01-12 08:40:11 -0300

    Removing sufix Integration from non integration test

--- src/test/java/com/cognite/client/CogniteClientTest.java
+++ src/test/java/com/cognite/client/CogniteClientTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CogniteClientIntegrationTest {
+class CogniteClientTest {
 
     @Test
     void test_ofKey_config() {
