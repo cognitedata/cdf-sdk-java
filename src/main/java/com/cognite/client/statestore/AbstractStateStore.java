91e889f088821a81a2f15ec69466f938f4931a98
kjetilh33 <kjetil.halvorsen@cognite.com>
2022-08-26 21:24:08 +0200

    add background thread handling

--- src/main/java/com/cognite/client/statestore/AbstractStateStore.java
+++ src/main/java/com/cognite/client/statestore/AbstractStateStore.java
@@ -2,11 +2,12 @@ package com.cognite.client.statestore;
 
 import com.google.protobuf.Struct;
 import com.google.protobuf.util.Values;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ConcurrentSkipListSet;
+import java.util.concurrent.*;
 
 /**
  * Abstract parent class for all state store implementations.
@@ -14,11 +15,25 @@ import java.util.concurrent.ConcurrentSkipListSet;
  * {@inheritDoc}
  */
 public abstract class AbstractStateStore implements StateStore {
+    protected static final String COLUMN_KEY_LOW = "low";
+    protected static final String COLUMN_KEY_HIGH = "high";
+    protected static final Duration DEFAULT_MAX_UPLOAD_INTERVAL = Duration.ofSeconds(10L);
 
-    protected final String COLUMN_KEY_LOW = "low";
-    protected final String COLUMN_KEY_HIGH = "high";
+    protected final Logger LOG = LoggerFactory.getLogger(this.getClass());
     protected ConcurrentMap<String, Struct> stateMap = new ConcurrentHashMap<>();
     protected Set<String> modifiedEntries = new ConcurrentSkipListSet<>();
+
+    protected final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+    protected ScheduledFuture<?> recurringTask;
+
+    {
+        // Make sure the thread pool puts its threads to sleep to allow the JVM to exit without manual
+        // clean-up from the client.
+        executor.setKeepAliveTime(2000, TimeUnit.SECONDS);
+        executor.allowCoreThreadTimeOut(true);
+        executor.setRemoveOnCancelPolicy(true);
+        System.out.println("Initializer block.");
+    }
 
     /**
      * {@inheritDoc}
@@ -96,15 +111,63 @@ public abstract class AbstractStateStore implements StateStore {
     @Override
     public abstract void commit() throws Exception;
 
-    /**
-     * {@inheritDoc}
+    /*
+    Private convenience method to decorate async commit with exception handling.
      */
-    @Override
-    public abstract boolean start();
+    private void asyncCommitWrapper() {
+        String logPrefix = "asyncCommitWrapper() - ";
+        try {
+            commit();
+        } catch (Exception e) {
+            LOG.error(logPrefix + "Exception during commit of the state store {}", e);
+            throw new RuntimeException(e);
+        }
+    }
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public abstract boolean stop();
+    public boolean start() {
+        String logPrefix = "start() - ";
+        if (null != recurringTask) {
+            // The upload task has already been started.
+            LOG.warn(logPrefix + "The commit thread has already been started. Start() has no effect.");
+            return false;
+        }
+
+        recurringTask = executor.scheduleAtFixedRate(this::asyncCommitWrapper,
+                1, DEFAULT_MAX_UPLOAD_INTERVAL.getSeconds(), TimeUnit.SECONDS);
+        LOG.info(logPrefix + "Starting background thread to commit state at interval {}", DEFAULT_MAX_UPLOAD_INTERVAL);
+        return true;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean stop() {
+        String logPrefix = "stop() -";
+        if (null == recurringTask) {
+            // The upload task has not been started.
+            LOG.warn(logPrefix + "The upload thread has not been started. Stop() has no effect.");
+            asyncCommitWrapper();
+            return false;
+        }
+
+        recurringTask.cancel(false);
+        boolean returnValue = recurringTask.isDone();
+        if (recurringTask.isDone()) {
+            // cancellation of task was successful
+            recurringTask = null;
+            asyncCommitWrapper();
+            LOG.info(logPrefix + "Successfully stopped the background commit thread.");
+        } else {
+            LOG.warn(logPrefix + "Something went wrong when trying to stop the commit thread. Status isDone: {}, "
+                            + "isCanceled: {}",
+                    recurringTask.isDone(),
+                    recurringTask.isCancelled());
+        }
+        return returnValue;
+    }
 }
