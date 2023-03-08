8f798451cb2b8c7caf4c6f441392b583d104940c
kjetilh33 <kjetil.halvorsen@cognite.com>
2023-03-08 22:33:28 +0100

    Finalize first draft

--- src/main/java/com/cognite/client/CdfHttpRequest.java
+++ src/main/java/com/cognite/client/CdfHttpRequest.java
@@ -16,12 +16,21 @@
 
 package com.cognite.client;
 
+import com.cognite.client.servicesV1.ResponseBinary;
+import com.cognite.client.servicesV1.executor.RequestExecutor;
 import com.google.api.Http;
 import com.google.auto.value.AutoValue;
+import com.google.common.base.Preconditions;
+import okhttp3.Headers;
+import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.Nullable;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -47,31 +56,69 @@ public abstract class CdfHttpRequest extends ApiBase {
      * This method is intended for internal use--SDK clients should always use {@link CogniteClient}
      * as the entry point to this class.
      *
+     * The apiPath parameter should represent the path segments following {@code https://<cdfHost>/api/v1/projects/{project}/}.
+     * For example, if you want to send a request to {@code https://<cdfHost>/api/v1/projects/{project}/}
+     *
      * @param client The {@link CogniteClient} to use for configuration settings.
+     * @param requestUrl The (CDF) URL to send the request to.
      * @return The datasets api object.
      */
-    public static CdfHttpRequest of(CogniteClient client, String apiPath) {
+    public static CdfHttpRequest of(CogniteClient client, URL requestUrl) {
         return CdfHttpRequest.builder()
                 .setClient(client)
-                .setApiPath(apiPath)
-                .build();
+                .setUrl(requestUrl)
+                .setRequestBody("")
+                .setRequestExecutor(RequestExecutor.of(client.getHttpClient())
+                        .withExecutor(client.getExecutorService())
+                        .withMaxRetries(client.getClientConfig().getMaxRetries()))
+                .build()
+                .withHeader("Accept", "application/json");
     }
 
     abstract CdfHttpRequest.Builder toBuilder();
-    abstract String getApiPath();
-    @Nullable
-    abstract Request getRequestBody();
+    abstract URL getUrl();
+    abstract String getRequestBody();
+    abstract RequestExecutor getRequestExecutor();
 
     public CdfHttpRequest withHeader(String key, String value) {
         headers.put(key, value);
         return this;
     }
 
+    public CdfHttpRequest withRequestBody(String requestBody)  {
+        return toBuilder().setRequestBody(requestBody).build();
+    }
+
+    public CdfHttpRequest withRequestBody(Request request) throws Exception {
+        return withRequestBody(request.getRequestParametersAsJson());
+    }
+
+    public ResponseBinary get() throws Exception {
+        okhttp3.Request request = buildGenericRequest().get().build();
+        return getRequestExecutor().executeRequest(request);
+    }
+
+    public ResponseBinary post() throws Exception {
+        okhttp3.Request request = buildGenericRequest()
+                .post(RequestBody.Companion.create(getRequestBody(), MediaType.get("application/json")))
+                .build();
+
+        return getRequestExecutor().executeRequest(request);
+    }
+
+    private okhttp3.Request.Builder buildGenericRequest() {
+        okhttp3.Request.Builder reqBuilder = new okhttp3.Request.Builder()
+                .headers(Headers.of(headers))
+                .url(HttpUrl.parse(getUrl().toString()));
+
+        return reqBuilder;
+    }
 
     @AutoValue.Builder
     abstract static class Builder extends ApiBase.Builder<Builder> {
-        abstract Builder setApiPath(String value);
-        abstract Builder setRequestBody(Request value);
+        abstract Builder setUrl(URL value);
+        abstract Builder setRequestBody(String value);
+        abstract Builder setRequestExecutor(RequestExecutor value);
 
         abstract CdfHttpRequest build();
     }
